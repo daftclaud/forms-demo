d3a52b540686e88072cf3c13f0054614f2404e93
daftclaud <cdsmileyone@gmail.com>
2022-12-03 12:42:33 -0600

    adds player-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { TeamDetailComponent } from './team-detail/team-detail.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    TeamDetailComponent
+    TeamDetailComponent,
+    PlayerDetailComponent
   ],
   imports: [
     BrowserModule,
